2a17db87e875b9e5dbcfb2ed33c51034db7218d7
Alexis BRIET <alexis.briet2003@gmail.com>
2023-12-18 20:07:10 +0100

    Update router with demo pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,14 +13,14 @@ const routes: Array<RouteRecordRaw> = [
     redirect: { name: 'dashboard' },
   },
   {
-    name: 'admin',
-    path: '/admin',
+    name: 'dashboard',
+    path: '/dashboard',
     component: AppLayout,
     children: [
       {
-        name: 'dashboard',
-        path: 'dashboard',
-        component: () => import('../pages/admin/dashboard/Dashboard.vue'),
+        name: 'home',
+        path: 'home',
+        component: () => import('../pages/admin/dashboard/Home.vue'),
       },
       {
         name: 'statistics',
@@ -181,6 +181,152 @@ const routes: Array<RouteRecordRaw> = [
         path: '',
         redirect: { name: 'login' },
       },
+    ],
+  },
+  {
+    name: 'demo',
+    path: '/demo',
+    component: AppLayout,
+    children: [
+      {
+        name: 'homeDemo',
+        path: 'home',
+        component: () => import('../pages/admin/dashboard/Dashboard.vue'),
+      },
+      {
+        name: 'statisticsDemo',
+        path: 'statistics',
+        component: RouteViewComponent,
+        children: [
+          {
+            name: 'charts',
+            path: 'charts',
+            component: () => import('../pages/admin/statistics/charts/Charts.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Charts',
+            },
+          },
+          {
+            name: 'progress-barsDemo',
+            path: 'progress-bars',
+            component: () => import('../pages/admin/statistics/progress-bars/ProgressBars.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Progress-Bars',
+            },
+          },
+        ],
+      },
+      {
+        name: 'formsDemo',
+        path: 'forms',
+        component: RouteViewComponent,
+        children: [
+          {
+            name: 'form-elementsDemo',
+            path: 'form-elements',
+            component: () => import('../pages/admin/forms/form-elements/FormElements.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/inputs',
+            },
+          },
+          {
+            name: 'medium-editorDemo',
+            path: 'medium-editor',
+            component: () => import('../pages/admin/forms/medium-editor/MediumEditor.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Medium-Editor',
+            },
+          },
+        ],
+      },
+      {
+        name: 'mapsDemo',
+        path: 'maps',
+        component: RouteViewComponent,
+        children: [
+          {
+            name: 'maplibre-mapsDemo',
+            path: 'maplibre-maps',
+            component: () => import('../pages/admin/maps/maplibre-maps/MapLibreMapsPage.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Maps',
+            },
+          },
+          {
+            name: 'yandex-mapsDemo',
+            path: 'yandex-maps',
+            component: () => import('../pages/admin/maps/yandex-maps/YandexMapsPage.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Maps',
+            },
+          },
+          {
+            name: 'leaflet-mapsDemo',
+            path: 'leaflet-maps',
+            component: () => import('../pages/admin/maps/leaflet-maps/LeafletMapsPage.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Maps',
+            },
+          },
+          {
+            name: 'bubble-mapsDemo',
+            path: 'bubble-maps',
+            component: () => import('../pages/admin/maps/bubble-maps/BubbleMapsPage.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Maps',
+            },
+          },
+          {
+            name: 'line-mapsDemo',
+            path: 'line-maps',
+            component: () => import('../pages/admin/maps/line-maps/LineMapsPage.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Maps',
+            },
+          },
+        ],
+      },
+      {
+        name: 'tablesDemo',
+        path: 'tables',
+        component: RouteViewComponent,
+        children: [
+          {
+            name: 'markup',
+            path: 'markup',
+            component: () => import('../pages/admin/tables/markup-tables/MarkupTables.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Tables',
+            },
+          },
+          {
+            name: 'dataDemo',
+            path: 'data',
+            component: () => import('../pages/admin/tables/data-tables/DataTables.vue'),
+            meta: {
+              wikiLink: 'https://github.com/epicmaxco/vuestic-admin/wiki/Tables',
+            },
+          },
+        ],
+      },
+      {
+        name: 'pagesDemo',
+        path: 'pages',
+        component: RouteViewComponent,
+        children: [
+          {
+            name: '404-pagesDemo',
+            path: '404-pages',
+            component: () => import('../pages/admin/pages/404PagesPage.vue'),
+          },
+          {
+            name: 'faqDemo',
+            path: 'faq',
+            component: () => import('../pages/admin/pages/FaqPage.vue'),
+          },
+        ],
+      },
+      UIRoute,
     ],
   },
   {
